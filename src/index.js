f2ab698026adb618de30b4640c03d50dc566ff3b
Daniel Molina-Power <danielmolina-power@MacUsers-MacBook-Air.local>
2020-09-18 14:24:29 -0700

    added firebase on index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA0-6Hfs-mPBB5Xyl3GyZ3tPPzDfSdEhaI",
+  authDomain: "artscape-97160.firebaseapp.com",
+  databaseURL: "https://artscape-97160.firebaseio.com",
+  projectId: "artscape-97160",
+  storageBucket: "artscape-97160.appspot.com",
+  messagingSenderId: "706465248594",
+  appId: "1:706465248594:web:bfae4b511225986cdd768b",
+  measurementId: "G-58YLW8N1N8"
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>

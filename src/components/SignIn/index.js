02e7cd214a9cf1f381ede4059dc50fc8a33a1ae2
Daniel Molina-Power <danielmolina-power@MacUsers-MacBook-Air.local>
2020-10-02 14:52:42 -0700

    added new styles to signin and signup pages

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import { Link } from "@reach/router";
 import { signInWithGoogle } from "../../fire";
 import { auth } from "../../fire";
-
+import "./style.css"
 
 const SignIn = () => {
 
@@ -31,17 +31,17 @@ const SignIn = () => {
    
 
   return (
-    <div className="mt-8">
-      <h1 className="text-3xl mb-2 text-center font-bold">Sign In</h1>
-      <div className="border border-blue-400 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
-        {error !== null && <div className = "py-4 bg-red-600 w-full text-white text-center mb-3">{error}</div>}
-        <form className="">
+  
+    <section className="login">
+      { <div className="border border-blue-400 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
+        {error !== null && <div className = "py-4 bg-red-600 w-full text-white text-center mb-3">{error}</div>} 
+        <div className="loginContainer">
+      <h1 className="signin">Sign In</h1>
           <label htmlFor="userEmail" className="block">
             Email:
           </label>
           <input
             type="email"
-            className="my-1 p-1 w-full"
             name="userEmail"
             value = {email}
             placeholder="E.g: faruq123@gmail.com"
@@ -53,39 +53,39 @@ const SignIn = () => {
           </label>
           <input
             type="password"
-            className="mt-1 mb-3 p-1 w-full"
             name="userPassword"
             value = {password}
             placeholder="Your Password"
             id="userPassword"
             onChange = {(event) => onChangeHandler(event)}
           />
-          <button className="bg-green-400 hover:bg-green-500 w-full py-2 text-white" onClick = {(event) => {signInWithEmailAndPasswordHandler(event, email, password)}}>
+          <button  onClick = {(event) => {signInWithEmailAndPasswordHandler(event, email, password)}}>
             Sign in
           </button>
-        </form>
-        <p className="text-center my-3">or</p>
-        <button
-          className="bg-red-500 hover:bg-red-600 w-full py-2 text-white"
+        <p className="or">or</p>
+        <button className="googlebutton"
           onClick={() => {
             signInWithGoogle();
           }}
         >
           Sign in with Google
         </button>
-        <p className="text-center my-3">
+        <p className="or">
           Don't have an account?{" "}
-          <Link to="signup" className="text-blue-500 hover:text-blue-600">
+          <Link to="signup">
             Sign up here
           </Link>{" "}
           <br />{" "}
-          <Link to="passwordReset" className="text-blue-500 hover:text-blue-600">
+          <Link to="passwordReset" >
             Forgot Password?
           </Link>
         </p>
       </div>
-    </div>
-  );
+        </div>
+   }
+    </section>
+   )
+  
 };
 
 export default SignIn;
--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -36,21 +36,20 @@ const Signup = () => {
   };
 
   return (
-    <div className="mt-8">
-      <h1 className="text-3xl mb-2 text-center font-bold">Sign Up</h1>
+    <section className="login">
       <div className="border border-blue-400 mx-auto w-11/12 md:w-2/4 rounded py-8 px-4 md:px-8">
         {error !== null && (
           <div className="py-4 bg-red-600 w-full text-white text-center mb-3">
             {error}
           </div>
         )}
-        <form className="">
+        <div className="loginContainer">
+        <h1 className="signin">Sign Up</h1>
           <label htmlFor="displayName" className="block">
             Display Name:
           </label>
           <input
             type="text"
-            className="my-1 p-1 w-full "
             name="displayName"
             value={displayName}
             placeholder="E.g: Faruq"
@@ -89,8 +88,7 @@ const Signup = () => {
           >
             Sign up
           </button>
-        </form>
-        <p className="text-center my-3">or</p>
+        <p className="or">or</p>
         <button
           onClick={() => {
             try {
@@ -99,18 +97,19 @@ const Signup = () => {
               console.error("Error signing in with Google", error);
             }
           }}
-          className="bg-red-500 hover:bg-red-600 w-full py-2 text-white"
+          className="googlebutton"
         >
           Sign In with Google
         </button>
-        <p className="text-center my-3">
+        <p className="or">
           Already have an account?{" "}
           <Link to="/" className="text-blue-500 hover:text-blue-600">
             Sign in here
           </Link>{" "}
         </p>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 

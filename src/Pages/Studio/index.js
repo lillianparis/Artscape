98949f0eedc5366a915e71c61947a8dbb6088ac5
Daniel Molina-Power <danielmolina-power@MacUsers-MacBook-Air.local>
2020-11-23 19:25:21 -0700

    added card for studio

--- src/Pages/Studio/index.js
+++ src/Pages/Studio/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import StudioHeader from "../../components/StudioHeader"
+import StudioHeader from "../../components/StudioHeader";
+import Card from "../../components/Cards";
 import "./style.css";
 
 function studio() {
   return (
+    <>
     <StudioHeader />
-    
+    <Card />
+    </>
   );
 }
 
--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -4,7 +4,14 @@ import "./style.css";
 function cards () {
     return (
 <>
-
+<div class="card">
+  <img src="https://images.unsplash.com/photo-1560160990-1c830b639bfd?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTV8fHBhaW50aW5nJTIwY2FudmFzfGVufDB8fDB8&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Artwork Name</h5>
+    <p class="card-text">Artwork price and info.</p>
+    <a href="#" class="btn btn-primary">Buy Now </a>
+  </div>
+</div>
 </>
     );
 }

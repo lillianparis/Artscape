99388ecd92978dd6810af2698f70cbd8655e3ec1
Daniel Molina-Power <danielmolina-power@MacUsers-MacBook-Air.local>
2020-11-26 11:27:49 -0700

    added marketing layout to homepage

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Carousel from "../../components/Carousel/index";
+import Marketing from "../../components/marketing";
 import "./style.css";
 
 function home() {
   return (
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
       <Carousel />
+      <Marketing />
     </main>
   );
 }

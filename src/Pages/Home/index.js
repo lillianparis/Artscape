797a50597b498cbe6f7ac79ba356fa725cf5cabf
Daniel Molina-Power <danielmolina-power@MacUsers-MacBook-Air.local>
2020-11-26 11:42:11 -0700

    footer added

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Carousel from "../../components/Carousel/index";
 import Marketing from "../../components/marketing";
+import Footer from "../../components/Footer";
 import "./style.css";
 
 function home() {
@@ -8,6 +9,7 @@ function home() {
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
       <Carousel />
       <Marketing />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function footer() {
+    return (
+
+<footer class="container">
+    <p class="float-right"><a href="#">Back to top</a></p>
+    <p>&copy; 2020 Artscape. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+  </footer>
+    );
+}
+
+export default footer;

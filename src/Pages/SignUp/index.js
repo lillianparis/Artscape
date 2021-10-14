5b5810ac94eb1d3e78c2ebe451ac196dea4c5435
Lillian Paris <lillian.paris529@gmail.com>
2021-10-14 10:03:29 -0700

    Trying to troubleshoot index.js does not match the corresponding disk error

--- src/Pages/SignUp/index.js
+++ src/Pages/SignUp/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Signup from './../../components/SignUp';
+import Signup from './../../components/SignUp/index';
 
 const Registration = props => {
     return <Signup />;

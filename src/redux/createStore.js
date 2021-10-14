0fbed4e79e5b334e02854c868a00f3d850d5424e
daniel molina <bigdpower@icloud.com>
2021-10-14 10:42:11 -0700

    added redux extension for chrom

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -3,6 +3,7 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import createSagaMiddle from 'redux-saga';
 import { persistStore } from 'redux-persist';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './rootReducer';
 import rootSaga from './rootSaga';
@@ -10,7 +11,7 @@ import rootSaga from './rootSaga';
 const sagaMiddleware = createSagaMiddle();
 export const middlewares = [thunk, sagaMiddleware, logger];
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
